149010e5088473800a9d1659c81c33c530eefdf0
Oskar Dadan <oskar.dadan@gmail.com>
2021-03-10 23:52:04 +0100

    impr: mockup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './components/welcome/welcome.component';
+
+const routes: Routes = [
+  { path: '**', component: WelcomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { TechComponent } from './components/tech/tech.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { HobbyComponent } from './components/hobby/hobby.component';
+import { ProjectComponent } from './components/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    NavbarComponent,
+    TimelineComponent,
+    TechComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    HobbyComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
